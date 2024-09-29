927fcbbfe5b9c5d7add226126c769f7519286efd
TUSHAR292004 <tusharchandak2004@gmail.com>
2024-09-29 16:55:51 +0530

    commit

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -17,11 +17,15 @@ const upload = multer({
 
 
 // /api/my/resturant
-router.post("/", 
-    upload.single("imageFile"),
-    validateMyRestaurantRequest,
-    jwtCheck,
-    jwtParse,
-    MyRestaurantController.createMyRestaurant);
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
 
 export default router;
